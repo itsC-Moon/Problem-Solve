167677e5fb56bee6ccdb533b77db756bcc0e3fd9
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-06-12 12:19:13 +0100

    add problem

---
+++ leetcode/509-Fibonacci-Number.cpp
@@ -0,0 +1,36 @@
+/*
+
+509-Fibonacci-Number
+Easy
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+*/
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        int x = 0, y = 1, z;
+
+        if (n < 2)
+            return n;
+        for (size_t i = 0; (int)i < n-1; i++)
+        {
+            z = x + y;
+            x = y;
+            y = z;
+        }
+        return z;
+    }
+};
+int main()
+{
+    Solution s1;
+    cout << s1.fib(12) << '\n';
+
+    return 0;
+}
--- leetcode/README.md
+++ leetcode/README.md
@@ -1,1 +1,3 @@
 Some of my solution to some leetcode problem 
+My Leetcode Profile
+https://leetcode.com/C_Moon/
